b35896ebd71696131d0ce81051f08f928f4b0c53
EliaColleoni <elia.colleoni@studenti.liceomascheroni.it>
2025-05-07 12:27:20 +0200

    Creazione Giocatore
    Creazione classe giocatore con attributi e costruttore

---
+++ include/giocatori.h
@@ -0,0 +1,16 @@
+#ifndef GIOCATORI_H
+#define GIOCATORI_H
+
+#pragma once
+
+class giocatori
+{
+public:
+    giocatori();
+    ~giocatori();
+
+private:
+
+};
+
+#endif
---
+++ src/giocatori.cpp
@@ -0,0 +1,11 @@
+#include "giocatori.h"
+
+giocatori::giocatori()
+{
+
+}
+
+giocatori::~giocatori()
+{
+
+}
