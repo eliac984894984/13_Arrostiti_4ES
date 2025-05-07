50267ab3bb7b4d0cafbe3f4aecd69f7bb0ec3f19
EliaColleoni <elia.colleoni@studenti.liceomascheroni.it>
2025-05-07 12:38:36 +0200

    correzione errori
    correzione errori cartelle

--- src/giocatori.cpp
+++ src/giocatori.cpp
@@ -1,16 +1,11 @@
 #include "giocatori.h"
-#include <string>
-#include <iostream>
-using namespace std;
 
-int const segnalino = 4;
-class Giocatore
+giocatori::giocatori()
 {
-private:
-    string colore; // colore coniglio (pedina)
-    string nome;
-public: 
-    Giocatore( string c, string n) : colore(c), nome(n) {}
-    
 
-};
+}
+
+giocatori::~giocatori()
+{
+
+}
