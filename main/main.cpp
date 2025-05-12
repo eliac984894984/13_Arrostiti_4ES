f8263a448bc7e5d2ab6ff966c0bba94f2ef3ce39
EliaColleoni <elia.colleoni@studenti.liceomascheroni.it>
2025-05-12 22:00:04 +0200

    introduzione main
    main in una cartella apposta gia collegato alle altre cartelle

---
+++ main/main.cpp
@@ -0,0 +1,13 @@
+#include "../include/Giocatore.h"
+#include "../src/Giocatore.cpp"
+#include "../include/Tabellone.h"
+#include "../src/Tabellone.cpp"
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+    
+}
