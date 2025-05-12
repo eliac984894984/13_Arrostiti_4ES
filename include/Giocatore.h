ee046de41c72f4b658c3e9d99f57699cb9575285
EliaColleoni <elia.colleoni@studenti.liceomascheroni.it>
2025-05-12 18:30:57 +0200

    Inserimento classe Giocatore
    Quello fatto in precedenza era una semplice prova.
    -> aggiunta di classe Giocatore+ costruttore

---
+++ include/Giocatore.h
@@ -0,0 +1,18 @@
+#ifndef GIOCATORE_H
+#define GIOCATORE_H
+
+#pragma once
+
+class Giocatore
+{
+protected:
+    string nome;
+    string colore;
+
+public:
+    Giocatore(string n, string c);
+    ~Giocatore();
+    // metodi da inserire
+};
+
+#endif
---
+++ src/Giocatore.cpp
@@ -0,0 +1,15 @@
+#include "../include/Giocatore.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+Giocatore::Giocatore(string n, string c)
+{
+    n = nome;
+    c = colore;
+}
+
+Giocatore::~Giocatore()
+{
+
+}
