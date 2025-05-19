806089a8b57c8ee69e1c783ca7c106db8d05742f
EliaColleoni <elia.colleoni@studenti.liceomascheroni.it>
2025-05-19 21:05:45 +0200

    Aggiunta prime carte
    prima descrizione delle carte
    (Circa dieci credo)

--- src/Carta.cpp
+++ src/Carta.cpp
@@ -1,11 +1,24 @@
-#include "Carta.h"
+#include "../include/Carta.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
 
-Carta::Carta()
-{
+using namespace std;
 
-}
-
-Carta::~Carta()
-{
-
+void Carta::inizializza() {
+    carte.push_back("Guscio di lumaca con un apertura.");
+    carte.push_back("Paesaggio verde con una scala a chiocciola al centro.");
+    carte.push_back("Ragazzo sull'altalena appesa ad un ramo.");
+    carte.push_back("Sfilata su tappeto rosso di guardie inglesi.");
+    carte.push_back("Sole che illumina un paesaggio variegato di colori.");
+    carte.push_back("Una serie di maschere appese con diverse espressioni.");
+    carte.push_back("Goccie che presentano diversi paesaggi al loro interno.");
+    carte.push_back("Pancake impilati con due formiche sopra.");
+    carte.push_back("Due lumache che si toccano la testa.");
+    carte.push_back("oggetto bianco circondato da fiori rossi.");
+    carte.push_back("Ragazza cinese con due omini sulle spalle.");
+    carte.push_back("Due scarpe col tacco rosa che sono sfumate.");
+    carte.push_back("Due porte rossa e blu eun coniglio che non sa dove entrare.");
+    carte.push_back("");
 }
