3ebb7a8a5cd26682f32c4a105e0d7eb61a63c371
gloriadevita <gloria.devita@studenti.liceomascheroni.it>
2025-05-20 10:57:38 +0200

    Regole 2
    forse finito

--- src/Regole.cpp
+++ src/Regole.cpp
@@ -4,13 +4,35 @@ using namespace std;
 void Regole::mostra(){
 
     cout<< "=== Regole del gioco ===" <<endl;
-    cout<< "Scopo:Il gioco che ha ricevuto molta fortuna e ancora tutt'ora è uno dei più famosi, ha come scopo quello di far 
+    cout<< "Scopo: Il gioco che ha ricevuto molta fortuna e ancora tutt'ora è uno dei più famosi, ha come scopo quello di far 
     indovinare agli altri giocatori qual è la carta descritta dal narratore, così da poter conquistare punti che ti permetteranno 
     di svolgere un percorso che ti porterà alla vittoria!" << endl;
-    cout<< "Tabellone:Il tabellone se pur presente in tutti i giochi da tavolo, ha un ruolo molto importante e significativo 
-    in quanto permette ai vari giocatori, che partiranno sulla medesima casella che corrisponde allo zero e all'inizio,
+    cout<< "Tabellone: Il tabellone se pur presente in tutti i giochi da tavolo, ha un ruolo molto importante e significativo 
+    in quanto permette ai vari giocatori, che partiranno sulla medesima casella che corrisponde al numero uno e all'inizio,
     di raggiungere la fine del percorso. Ogni giocatore viene rappresentato da una pedina differente dagli altri 
     partecipanti (puoi osservare ciò nella sezione Tabellone),per far si di tenere il proprio spostamento e il percordo 
     fino a raggiungere la fine che corrisponde al numero 30." <<endl;
+    cout<<"Giocatori: Nell'edizione classica di Dixit il massimo di giocatori che si possono avere sono 6 ed è invece caldamente 
+    consigliato di stare nei margini indicati per l'età, quindi dagli 8 anni in su. Tutto ciò poi può variare in base alle varie 
+    tipologie a cui si decide di giocatore esempio: tipologia disney, tipologia stellare, tipologia Odyssey...
+    Prova le varie edizioni, cosa aspetti?!" <<endl;
+    cout<<"Punti: Ci sono diversi modi per conquistare punti. Tutto inizia con la distribuzione di 6? carte a partecipante e dei 
+    segnalini voto dal numero 1 al numero corrispondente a quello dei giocatori; a turno viene scelto un narratore, che seleziona 
+    una carta dalla propria mano e la descrive in modo vago e non troppo esplicito agli altri giocatori in modo che possano 
+    risalire a quale carta si tratta. Fatto ciò, ogni giocatore dovrà scegliere dalla propria mano la carta che ritiene corretta 
+    e darla coperta al narratore, il quale mescolerà le carte dei partecipanti per poi posizionarle sul tavolo scoperte. Ora tutti
+    i giocatori (narratore escluso) posizionano una carta segnalino coperta con il numero corrispondente alla carta che credono 
+    sia quella del narratore e attendono che quest'ultimo riveli tutte le carte. Tutti coloro che hanno indovinato ricevono 
+    3 punti e, se almeno una persona ha avuto successo, anche il narratore riceve 3 punti. Se nessuno ha indovinato o al contrario,
+    se tutti lo hanno fatto, allora ciascun giocatore riceve 2 punti mentre il narratore 0 . Il narratore, quindi, deve stare 
+    attento nel fornire un indizio che non sia né troppo criptico né troppo esplicito, in modo da assicurarsi che almeno una 
+    persona indovini la sua carta. Inoltre, ogni giocatore ad esclusione del narratore ottiene 1 punto extra per ogni voto ricevuto
+    dalla propria carta. Al termine della manche tutti i giocatori pescano una carta così da averne sempre in mano sei, mentre il 
+    ruolo del narratore passa al giocatore a sinistra." <<endl;
+    cout<<"Vittoria: Una volta giunti alla fine del gioco, vince chi ha raggiunto per primo i 30 punti e quindi ha raggiunto il 
+    traguardo nel tabellone. perciò gioca e prova a vincere, in questo magnifico gioco e in tutte le sue edizioni che esistono 
+    (vai e scopri di più su tutte le differenti edizioni)." <<endl;
+
+    return 0;
 
 }
