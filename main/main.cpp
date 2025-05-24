6ca51e6310884d9bae3358e93ce385c2f8e870f5
sangalettigaia4ES <gaia.sangaletti@studenti.liceomascheroni.it>
2025-05-24 17:15:34 +0200

    creazione oggetto carta nel main

--- main/main.cpp
+++ main/main.cpp
@@ -16,7 +16,9 @@ int main(){
     string nomeGiocatore, colore1, colore2, colore3, colore4;
     int scelta;
     int numGiocatori=4;
-    
+
+    Carta c;
+    c.inizializza();
     
     cout << "i colori delle pedine disponibili sono blu, rosso, giallo e verde. Non è possibile per due giocatori scegliere la stessa pedina " << endl;
 
