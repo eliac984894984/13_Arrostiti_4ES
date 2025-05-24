f9bb42ef5341b456e3747c3da75417869cf45437
Martina00000 <martina.barbetta@studenti.liceomascheroni.it>
2025-05-24 19:30:35 +0200

    regole
    correzione

--- src/Regole.cpp
+++ src/Regole.cpp
@@ -5,7 +5,7 @@ void Regole::mostra() {
     cout << "=== Regole del gioco ===" << endl;
 
     cout << "Scopo: Il gioco, che ha ricevuto molta fortuna ed è ancora oggi uno dei più famosi, "
-         << "ha come scopo quello di far indovinare agli altri giocatori qual è la carta descritta dal narratore, "
+         << "ha come scopo quello di far indovinare agli altri giocatori qual'è la carta descritta dal narratore, "
          << "così da poter conquistare punti che ti permetteranno di svolgere un percorso che ti porterà alla vittoria!" << endl;
 
     cout << "Tabellone: Il tabellone, seppur presente in tutti i giochi da tavolo, ha un ruolo molto importante e significativo, "
@@ -13,12 +13,11 @@ void Regole::mostra() {
          << "di raggiungere la fine del percorso. Ogni giocatore è rappresentato da una pedina diversa (puoi osservare ciò nella sezione Tabellone), "
          << "per tenere traccia dei propri spostamenti fino alla fine, che corrisponde al numero 30." << endl;
 
-    cout << "Giocatori: Nell'edizione classica di Dixit il massimo numero di giocatori è 6, ed è caldamente consigliato "
+    cout << "Giocatori: Nell'edizione classica di Dixit il massimo numero di giocatori è 4, ed è caldamente consigliato "
          << "rispettare il limite di età indicato, ovvero dagli 8 anni in su. "
-         << "Tutto ciò può variare in base alle edizioni: Disney, Stellare, Odyssey... "
-         << "Provale tutte, cosa aspetti?!" << endl;
+         << "Tutto ciò può variare in base alle edizioni: Disney, Stellare, Odyssey... "<< endl;
 
-    cout << "Punti: Ci sono diversi modi per ottenere punti. Ogni giocatore riceve 6 carte e dei segnalini voto numerati. "
+    cout << "Punti: Ci sono diversi modi per ottenere punti. Ogni giocatore riceve 5 carte e dei segnalini voto numerati. "
          << "A turno viene scelto un narratore, che seleziona una carta dalla propria mano e la descrive in modo vago. "
          << "Gli altri giocatori scelgono una carta dalla propria mano che pensano possa corrispondere e la consegnano coperta. "
          << "Il narratore mescola tutte le carte e le mostra scoperte. Ora ogni giocatore (tranne il narratore) vota quella che ritiene corretta. "
