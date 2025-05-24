bc9e1ecd33893427a74871b69a4087861b3bd3e0
Martina00000 <martina.barbetta@studenti.liceomascheroni.it>
2025-05-24 10:25:16 +0200

    implementazione dei metodi della classe giocatore (GAIA)
    implementazione dei metodi controlloPedine e getColore (GAIA)
    
    Co-Authored-By: sangalettigaia4ES <211475861+sangalettigaia4ES@users.noreply.github.com>

--- src/Giocatore.cpp
+++ src/Giocatore.cpp
@@ -10,7 +10,17 @@ Giocatore::Giocatore(string n, string c)
 }
 
 
-bool Giocatore :: controlloPedine(string c) 
+void Giocatore :: controlloPedine() 
 {
-   
+   do{
+        if(colore!="blu" && colore!="rosso" && colore!="giallo" && colore!="verde")
+        {
+            cout<<"il colore inserito non e' valido. Inserire un nuovo colore"; 
+            cin >> colore; 
+        };
+   }while(colore!="blu" && colore!="rosso" && colore!="giallo" && colore!="verde");
+}
+
+string Giocatore :: getColore() {
+    return colore; 
 }
