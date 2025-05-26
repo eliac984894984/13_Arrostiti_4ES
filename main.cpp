c173aac1d01a89b8198662b8dbf3ada7c9232622
EliaColleoni <elia.colleoni@studenti.liceomascheroni.it>
2025-05-26 20:05:51 +0200

    update
    Co-Authored-By: gloriadevita <207993076+gloriadevita@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -116,6 +116,7 @@ int main(){
             m.votaCarte(numGiocatori, carte_tavolo, i, punti1, punti2, punti3, punti4, cartaNarratore); //i giocatori votano la presunta carta corretta
             m.scarta(carte_tavolo); //le carte sul tavolo vengono scartate e messe nel mazzo degli scarti
             m.pesca(numGiocatori); //la nuova carta viene pescata 
+            
             if(punti1<=30 && punti2<=30 && punti3<=30 && punti4<=30) break;
             narratore++; //il narratore cambia e diventa il giocatore successivo 
         }
