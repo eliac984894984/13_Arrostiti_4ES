6be49536138dca5dfe018f1e9a8ce47ca3ff69fa
EliaColleoni <elia.colleoni@studenti.liceomascheroni.it>
2025-05-26 16:23:18 +0200

    update

--- main/main.cpp
+++ main/main.cpp
@@ -24,49 +24,51 @@ int main(){
     Regole r; //creazione dell'oggetto regole 
     r.mostra(); //metodo per mostrare le regole
     
-    cout << "i colori delle pedine disponibili sono blu, rosso, giallo e verde. Non è possibile per due giocatori scegliere la stessa pedina " << endl;
+
+    cout <<endl;
+    cout << "i colori delle pedine disponibili sono blu, rosso, giallo e verde. Non è possibile per due giocatori scegliere la stessa pedina. " << endl;
 
     //primo giocatore
-    cout << "inserici il nome e colore della pedina del primo giocatore " << endl ;
+    cout << "inserici il nome e colore della pedina del primo giocatore: " << endl ;
     cin >> nomeGiocatore;
     cin >> colore1;
     Giocatore Giocatore1(nomeGiocatore, colore1);
     Giocatore1.controlloPedine(); //meccanismo per controllare che il colore sia valido
 
     //secondo giocatore
-    cout << "inserici il nome e colore della pedina del secondo giocatore " << endl ;
+    cout << "inserici il nome e colore della pedina del secondo giocatore: " << endl ;
     cin >> nomeGiocatore;
     do {
         cin >> colore2;
         if (Giocatore1.getColore()==colore2) //meccanismo per controllare che non sia stato lo stesso colore
         {
-            cout << "non possono essere scelte le pedine di uno stesso colore, inserire il nuovo colore" << endl;
+            cout << "non possono essere scelte le pedine di uno stesso colore, inserire il nuovo colore: " << endl;
         }
     } while(Giocatore1.getColore()==colore2);
     Giocatore Giocatore2(nomeGiocatore, colore2);
     Giocatore2.controlloPedine(); //meccanismo per controllare che il colore sia valido
     
     //terzo giocatore
-    cout << "inserici il nome e colore della pedina del secondo giocatore " << endl ;
+    cout << "inserici il nome e colore della pedina del secondo giocatore: " << endl ;
     cin >> nomeGiocatore;
     do {
         cin >> colore3;
         if (Giocatore1.getColore()==colore3 || Giocatore2.getColore()==colore3) //meccanismoper controllare che non sia stato lo stesso colore
         {
-        cout << "non possono essere scelte le pedine di uno stesso colore, inserire il nuovo colore" << endl;
+        cout << "non possono essere scelte le pedine di uno stesso colore, inserire il nuovo colore: " << endl;
         }
     } while(Giocatore1.getColore()==colore3 || Giocatore2.getColore()==colore3);
     Giocatore Giocatore3(nomeGiocatore, colore3);
     Giocatore3.controlloPedine(); //meccanismo per controllare che il colore sia valido
 
     //quarto giocatore
-    cout << "inserici il nome e colore della pedina del secondo giocatore " << endl ;
+    cout << "inserici il nome e colore della pedina del secondo giocatore: " << endl ;
     cin >> nomeGiocatore;
     do {
         cin >> colore4;
         if (Giocatore1.getColore()==colore4 || Giocatore2.getColore()==colore4 || Giocatore3.getColore()==colore4) //meccanismo per controllare che non sia stato lo stesso colore
         {
-            cout << "non possono essere scelte le pedine di uno stesso colore, inserire il nuovo colore" << endl;
+            cout << "non possono essere scelte le pedine di uno stesso colore, inserire il nuovo colore: " << endl;
         }
     } while(Giocatore1.getColore()==colore4 || Giocatore2.getColore()==colore4 || Giocatore3.getColore()==colore4);
     Giocatore Giocatore4(nomeGiocatore, colore4);
